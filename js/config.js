b93b4982e8a880ee8a8028743341872d3fac6af1
lilithevelynn <63135482+lilithevelynn@users.noreply.github.com>
2020-04-23 00:18:48 +0300

    loadingscreen

--- js/config.js
+++ js/config.js
@@ -44,7 +44,7 @@ Avaiable options:
 
 You can add custom backgrounds
 Just make sure it is at webm VP8 format */
-var l_background = "backgrounds/videos/glimmering.webm";
+var l_background = "backgrounds/videos/bluefall.webm";
 
 /*(Image-background only)
 Place one or more images
